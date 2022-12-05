880cfbdee0e29e3008fe55549df91c96db7c264b
Aykuayvenchorr <nastyashevchenkomail@mail.ru>
2022-12-05 18:47:11 +0300

    token added

--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -3,11 +3,13 @@ from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
 # TODO подключите UserViewSet из Djoser.views к нашим urls.py
 # TODO для этокого рекоммендуется использовать SimpleRouter
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 user_router = SimpleRouter()
 user_router.register("users", UserViewSet, basename="users")
 
 urlpatterns = [
     path("", include(user_router.urls)),
-
+    path("token/", TokenObtainPairView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
 ]
